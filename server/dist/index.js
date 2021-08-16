61a50d04c6ddeb046a130d32d7edcd123bfc0bc8
Alex Romo III <alexromo3rd@gmail.com>
2021-08-16 16:11:55 -0500

    Working on backend

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config({ path: '../.env' });
 const express_1 = __importDefault(require("express"));
-const { SERVER_PORT } = process.env;
+const mongoose_1 = __importDefault(require("mongoose"));
+const { SERVER_PORT, MONGO_URI } = process.env;
 const app = express_1.default();
 app.use(express_1.default.json());
+mongoose_1.default.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose_1.default.connection;
+console.log(db);
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running on port ${SERVER_PORT}.`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,19 @@
 require('dotenv').config({ path: '../.env' });
 import express from 'express';
+import mongoose from 'mongoose';
 
-const { SERVER_PORT } = process.env;
-
+const { SERVER_PORT, MONGO_URI } = process.env;
 const app = express();
 app.use(express.json());
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+console.log(db);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on port ${SERVER_PORT}.`);
